49fe137b515a77d7c6fa79bffe8a08c67cd1d1e7
Karl Heinz Marbaise <kama@soebes.de>
2020-12-02 19:30:49 +0100

    WIP: First implementation of beforeEach copying action with check for expected elements in target integration test directory.

--- itf-examples/src/test/java/com/soebes/itf/examples/ITWithBeforeEachMavenIT.java
+++ itf-examples/src/test/java/com/soebes/itf/examples/ITWithBeforeEachMavenIT.java
@@ -19,19 +19,54 @@ package com.soebes.itf.examples;
  * under the License.
  */
 
+import com.soebes.itf.extension.assertj.MavenITAssertions;
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
 import com.soebes.itf.jupiter.extension.MavenTest;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
 import com.soebes.itf.jupiter.maven.MavenProjectResult;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @MavenJupiterExtension
 class ITWithBeforeEachMavenIT {
 
   @BeforeEach
   void beforeEach(MavenProjectResult project) {
+    // Verzeichnisse prüfen, ob die da sind...
+    String base = this.getClass().getResource("/").getFile();
+    File xdirectory = new File(base, "com/soebes/itf/examples/ITWithBeforeEachMavenIT/the_first_test_case");
+    List<String> expectedElements = createElements(xdirectory);
+
+    expectedElements.forEach(s -> System.out.println("s = " + s));
     System.out.println("* beforeEach of ITWithBeforeEachIT");
     System.out.println("project = " + project.getBaseDir());
+
+    File projectDirectory = new File(project.getBaseDir(), "project");
+
+    MavenITAssertions.assertThat(projectDirectory)
+        .isDirectory();
+//        .isDirectoryContaining(s -> s.getName().equals("pom.xml")).hasSize(1);
+    MavenITAssertions.assertThat(projectDirectory).isDirectoryNotContaining(s -> s.isDirectory() && s.getName().equals("target"));
+
+    List<String> actualElements = createElements(projectDirectory);
+
+    actualElements.forEach(s -> System.out.println("actual:" + s));
+    MavenITAssertions.assertThat(actualElements).containsExactlyInAnyOrderElementsOf(expectedElements);
+  }
+
+  private List<String> createElements(File xdirectory) {
+    Collection<File> expectedCollectedFiles = FileUtils.listFilesAndDirs(xdirectory, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+    expectedCollectedFiles.forEach(System.out::println);
+
+    List<String> expectedElements = expectedCollectedFiles.stream().map(p -> p.toString().replace(xdirectory.getAbsolutePath(), "")).collect(toList());
+    return expectedElements;
   }
 
   @MavenTest
